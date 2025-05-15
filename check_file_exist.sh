53d562273a9323019c98c1b0a1b88a5f956cc1d5
Abdoulkadri-hu <bakoabdoulkadri@gmail.com>
2025-05-15 11:44:52 +0100

    check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,7 +3,7 @@
 echo -n "Entrez le nom du fichier : "
 read filename
 
-if [ -f "$filename" ]; then
+if [ -f '$filename' ]; then
     echo "Le fichier \'$filename\' existe."
 else
     echo "Le fichier \'$filename\' n'existe pas."
